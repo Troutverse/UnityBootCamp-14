b1e4ab6b92063860b51d1d0fc80a423346bd91d0
TroutVerse <troutverse@gmail.com>
2025-09-11 15:50:24 +0900

    [20250911] Add file UpLoad

---
+++ csharp/20250911_2/20250911_2/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Add(1, 1);
+    }
+
+    static void Add(int Y, int X)
+    {
+        int[,] map =
+        {
+            { 1, 1, 1},
+            { 1, 1, 1},
+            { 1, 1, 1},
+        };
+
+        int[] deltaX = { 0, 1, 0, -1 };
+        int[] deltaY = { -1, 0, 1, 0 };
+
+        for (int i = 0; i < deltaX.Length; i++)
+        {
+            int nx = X + deltaX[i];
+            int ny = Y + deltaY[i];
+
+            map[ny, nx] += 1;
+        }
+
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
