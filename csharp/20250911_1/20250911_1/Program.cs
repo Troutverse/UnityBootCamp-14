38d3ed1989538bd953847e0de731dfdd3712d87a
TroutVerse <troutverse@gmail.com>
2025-09-11 09:43:15 +0900

    [20250911] Swap Example

---
+++ csharp/20250911_1/20250911_1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int q = 0, w = 1;
+        Swap(ref q, ref w);
+        Console.WriteLine(q + " " + w);
+    }
+
+    public static void Swap(ref int a, ref int b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+}
